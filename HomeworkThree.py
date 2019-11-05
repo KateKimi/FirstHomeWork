7219713bbc3275c9af4d2440c6fdc050f7b22b69
KateKimi <moskalkova.kate@gmail.com>
2019-11-05 12:57:37 +0200

    Add last part of homework

--- HomeworkThree.py
+++ HomeworkThree.py
@@ -1,5 +1,65 @@
 #word 'hello25'
 
-x = input("Enter text: ")
+# x = input("Enter text: ")
+# b = x[1:-1]
+# c = b[::-1]
+#
+# print(x[2], x[-2], x[0:5], x[::2], x[1::2], x[::-1], x[-1::-2], x[-2::-2], c, len(x), sep="\n")
 
-print(x[2], x[-2], x[0:5], x[::2], x[1::2], x[::-1], x[-1::-2], x[-2::-2], x[1:-1], len(x), sep="\n")
+
+# Second task in homework (third lesson)
+# x = int(input("Enter the year: "))
+# if ((x % 4 == 0) and (x % 100 !=0)) or (x % 400 == 0):
+#     print("Yes")
+# else:
+#      print("No")
+
+#Third task in homework
+# a = int(input("Enter a = "))
+# b = int(input("Enter b = "))
+# c = int(input("Enter c = "))
+# if (a + b) > c and (a + c) > b and (b + c) > a:
+#     print("Yes")
+# else:
+#     print("No")
+
+#fourth task in homework
+
+# a = int(input("Enter a = "))
+# b = int(input("Enter b = "))
+# c = int(input("Enter c = "))
+# if a < b < c:
+#     print(a, b, c)
+# elif a < c < b:
+#     print(a, c, b)
+# elif b < a < c:
+#     print(b, a, c)
+# elif b < c < a:
+#     print(b, c, a)
+# elif c < b < a:
+#     print(c, b, a)
+# else:
+#     print(c, a, b)
+
+#fifth task in homework
+
+# a = int(input("Enter a = "))
+# b = int(input("Enter b = "))
+# c = int(input("Enter c = "))
+#
+# if a == b == c:
+#     print(3)
+# elif (a == b != c) or (a == c != b) or (b == a != c) or (b == c != a) or (c == a != b) or (c == b != a):
+#     print(2)
+# else:
+#     print(0)
+
+#sixth task in homework
+#word hello , решение нашла в интернете, но так и не поняла почему так :(
+# x = input("Enter x = ")
+# print(x[(len(x) + 1) // 2:] + x[:(len(x) + 1) // 2])
+
+
+
+
+
