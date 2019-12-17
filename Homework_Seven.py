b2992cbefbb25f4c9c8113ec08dfc7760a0b9171
KateKimi <moskalkova.kate@gmail.com>
2019-12-17 16:42:44 +0200

    Seventh homework

--- Homework_Seven.py
+++ Homework_Seven.py
@@ -50,11 +50,11 @@ print(p1.years())
 
 """
 2) Employee (наследуемся от Person) (добавляются свойства: должность, опыт работы, зарплата)
-** Можете в конструкторе проверить, что в опыт работы и зарплата не отрицательные 
-Реализовать новые методы: 
-1.	возвращает должность с приставкой, которая зависит от опыта работы: Junior — менее 3 лет, Middle — от 3 до 6 лет, Senior — больше 6 лет. 
+** Можете в конструкторе проверить, что в опыт работы и зарплата не отрицательные
+Реализовать новые методы:
+1.	возвращает должность с приставкой, которая зависит от опыта работы: Junior — менее 3 лет, Middle — от 3 до 6 лет, Senior — больше 6 лет.
 Т.е. метод должен вернуть позицию с приставкой Junior/Middle/Senior <position>.
- Если, например у вас объект имел должность “programmer” с опытом 2 года, метод должен вернуть “Junior programmer” 
+ Если, например у вас объект имел должность “programmer” с опытом 2 года, метод должен вернуть “Junior programmer”
 2.	метод, который увеличивает зарплату на сумму, которую вы передаёте аргументом.
 
 """
@@ -71,6 +71,9 @@ class Employee(Person):
         if experience < 0:
             raise ValueError("Experiense can't be < 0")
 
+    def __str__(self):
+        return f"{self.__class__} Object: full_name={self.full_name}"
+
     def mon_position(self):
         experience = self.experience
         if experience < 3:
@@ -81,9 +84,118 @@ class Employee(Person):
             m_position = 'Senior'
         return f"{m_position} {self.position}"
     def mon_salary(self, money):
-        return self.salary * money
+        if type(money) == str or type(money) == list or type(money) == dict:
+            raise ValueError('Money must be int or float')
+        return self.salary + money
+
 
 e1 = Employee(full_name="Vasya Man", salary=100, age_birth=1987, experience=2, position='programmer')
 print(e1.full_name)
 print(e1.mon_position())
-print(e1.mon_salary(2))
+print(e1.mon_salary(35))
+
+"""
+3) ITEmployee (наследуемся от Employee)
+1. Реализовать метод добавления одного навыка в новое свойство skills (список) новым методом add_skill (см. презентацию).
+2. * Реализовать метод добавления нескольких навыков в новое свойство skills (список) новым методом add_skills.
+Тут можно выбрать разные подходы:
+или аргумент один и он список навыков, которым вы расширяете список-свойство skill,
+ или вы принимаете неопределённое количество аргументов, и все их добавляете в список-свойство skill
+"""
+class ITEmployee(Employee):
+    def __init__(self, full_name=" ", age_birth=None, salary=None, position=None, experience=None, skills=[]):
+        Employee.__init__(self, full_name, age_birth, salary, position, experience)
+        self.skills = skills
+
+    def __str__(self):
+        return f"{self.__class__} Object: full_name={self.full_name}, skills={self.skills}"
+
+    def add_skills(self, new_skill):
+        try:
+            self.skills.append(new_skill)
+        except AttributeError: self.skills = [new_skill]
+        return self.skills
+
+    def all_skills(self, *args):
+        for n in args:
+            try:
+                self.skills.extend(args)
+            except AttributeError: self.skills = [*args]
+            return self.skills
+
+
+
+it1 = ITEmployee(full_name="Petya Man", salary=1000, age_birth=1985, experience=2, position='programmer', skills=[])
+print(it1.full_name)
+print(it1.add_skills('Java'))
+print(it1.all_skills('More','OneMore'))
+print(it1.skills)
+print(it1.all_skills('Python','Ruby', 'ETC'))
+
+"""
+Задание 2 (на создание новых классов)
+Создать классы
+1) Прямоугольная площадка (пример: комната) (свойства: две стороны).
+Методы:
+1.	вычисляем площадь,
+2.	вычисляем периметр.
+3.	** сделайте возможность сливать две «комнаты» в одну операцией +
+
+"""
+class Room:
+    def __init__(self, one_side, second_side):
+        self.one_side = one_side
+        self.second_side = second_side
+
+    def __add__(self, other):
+        r = Room(self.one_side + other.one_side, self.second_side + other.second_side)
+        return r
+    def __str__(self):
+        return f"Room {self.one_side}, {self.second_side}"
+
+    def S_2(self):
+        return self.one_side * self.second_side
+    def P(self):
+        return ((self.one_side + self.second_side) * 2)
+
+first_room = Room(one_side=5, second_side= 10)
+
+print(first_room.S_2())
+print(first_room.P())
+room_1 = Room(one_side=5, second_side=10)
+room_2 = Room(one_side=8, second_side=9)
+
+all = room_1 + room_2
+print(all)
+
+"""
+2) Точка на карте (свойства: X, Y). 
+Методы:
+1.	Нужно вычислить расстояние до начала координат, 
+2.	* вычисляется расстояние между точкой и другой точкой экземпляром этого же класса
+3.	** перевод в другие системы координат
+"""
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        p = Point(self.x + other.x, self.y + other.y)
+        return p
+
+    def __str__(self):
+        return f"Point {self.x}, {self.y}"
+
+p0 = Point(x=0, y=0)
+p1 = Point(x=2, y=3)
+p2 = Point(x=4, y=6)
+
+#1
+rasstoyanie = p1 + p0
+print(rasstoyanie)
+
+#2
+p_two = p1 + p2
+print(p_two)
